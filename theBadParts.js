d93e38e52087a001ad0e31e07509f6af7959c570
Isaac Flaum <isaac@forensiq.com>
2015-12-17 02:07:46 -0500

    setting up skeleton

--- theBadParts.js
+++ theBadParts.js
@@ -1,14 +1,14 @@
-// variables in blocks are hoisted to the top of the function scope:
-// this
-if (true) {
-	var hoisted = 3;
-}
-// is actually this
-var hoisted2;
-if (true) {
-	hoisted2 = 3;
-}
-console.log(hoisted === hoisted2);
+// Douglas Crockford gave us some of the good parts,
+// now let me introduce some of the bad ones :)
+var expect = require('chai').expect;
 
-// not a number has the same type as number:
-console.log(typeof NaN === typeof 4.5);
+describe('typeof operator', function() {
+  it('typeof NaN is number', function () {
+  	expect(typeof NaN).to.equal(typeof 4);
+  });
+
+  it('typeof null is object', function () {
+  	expect(typeof null).to.equal(typeof {});
+  });
+
+});
