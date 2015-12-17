5ef14a38a5e5dc65b5f5fb285e9ac7fa27ca586c
Isaac Flaum <isaac@forensiq.com>
2015-12-17 16:23:47 -0500

    floating point weirdness

---
+++ floatingPoint.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('floating point weirdness', function () {
+	it('0.1 + 0.2 does not equal 0.3', function () {
+		expect(0.1 + 0.2).to.not.equal(0.3); // number
+	});
+});
