00895e7403f7a8b9c7a6f9781f8774066e38439d
Isaac Flaum <isaac@forensiq.com>
2015-12-18 01:11:01 -0500

    better formatting

--- doubleEquals.js
+++ doubleEquals.js
@@ -1,21 +1,21 @@
 var expect = require('chai').expect;
 
 describe('double equals operator', function () {
-	it('0, \'\',  and \'0\' transitivity problems', function () {
-		expect('' == '0').to.be.false;
-		expect(0 == '').to.be.true;
-		expect(0 == '0').to.be.true;
-	});
-	
-	it('false, null, and undefined transitivity problems', function () {
-		expect(false == undefined).to.be.false;
-		expect(false == null).to.be.false;
-		expect(null == undefined).to.be.true;
-	});
+    it('0, \'\',  and \'0\' transitivity problems', function () {
+        expect('' == '0').to.be.false;
+        expect(0 == '').to.be.true;
+        expect(0 == '0').to.be.true;
+    });
+    
+    it('false, null, and undefined transitivity problems', function () {
+        expect(false == undefined).to.be.false;
+        expect(false == null).to.be.false;
+        expect(null == undefined).to.be.true;
+    });
 
-	//this is also true with the triple equals operator
-	it('NaN does not equal itself', function () {
-		expect(NaN).to.not.equal(NaN);
-		expect(NaN != NaN).to.be.true;
-	});
+    //this is also true with the triple equals operator
+    it('NaN does not equal itself', function () {
+        expect(NaN).to.not.equal(NaN);
+        expect(NaN != NaN).to.be.true;
+    });
 });
--- typeof.js
+++ typeof.js
@@ -1,11 +1,11 @@
 var expect = require('chai').expect;
 
 describe('typeof operator', function () {
-  it('typeof NaN is number', function () {
-  	expect(typeof NaN).to.equal(typeof 4);
-  });
-  
-  it('typeof null is object', function () {
-  	expect(typeof null).to.equal(typeof {});
-  });
+	it('typeof NaN is number', function () {
+		expect(typeof NaN).to.equal(typeof 4);
+	});
+
+	it('typeof null is object', function () {
+		expect(typeof null).to.equal(typeof {});
+	});
 });
